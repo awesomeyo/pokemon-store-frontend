5ceffd08c11c420c0b5d06bb5ecea5fb807d6c52
yeo yeu lind <yeoyyl92@gmail.com>
2023-07-26 20:38:46 +0800

    add Navbar,Card,Sidebar compo

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { NavbarDefault } from "./components/Navbar"
+import { MultiLevelSidebar } from "./components/Sidebar"
+import {EcommerceCard} from "./components/Card"
+
+export default function App() {
+  return (
+    <> 
+    
+    <NavbarDefault />
+    <MultiLevelSidebar />
+    <EcommerceCard />
+    
+    </>
+   
+  );
+}
